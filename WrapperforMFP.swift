cb2c9b8fb7f90c5619c338ed1951bf4ecfcf3454
Raja Rahul <rajarahul@Rajas-MacBook-Pro.local>
2019-01-16 10:41:52 +0530

    Adding Files

---
+++ WrapperforMFP.swift
@@ -0,0 +1,18 @@
+
+//
+//  WrapperforMFP.swift
+//  Pods-WrapperforMFP_Tests
+//
+//  Created by Raja Rahul on 16/01/19.
+//
+
+import UIKit
+
+public class WrapperforMFP: UIView {
+
+    public func connectToMFPWrapper () -> String {
+        return "Hello";
+    }
+    
+
+}
